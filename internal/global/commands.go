e849cc7420365340d2d47ffea2c8071ce3dc35dc
Mmx233 <admin@mmxblog.com>
2023-04-12 20:06:30 +0800

    feat: 命令行参数解析

---
+++ internal/global/commands.go
@@ -0,0 +1,20 @@
+package global
+
+import (
+	"github.com/Mmx233/GoReleaseCli/internal/global/models"
+	"github.com/alecthomas/kingpin/v2"
+)
+
+var Commands models.Commands
+
+func InitCommands(version string) {
+	Commands.App = kingpin.New("release", "Golang build production release helper.")
+	Commands.App.Version(version)
+	Commands.App.VersionFlag.Short('v')
+	Commands.App.HelpFlag.Short('h')
+
+	Commands.App.Arg("target", "target package").Default(".").StringVar(&Commands.Target)
+	Commands.App.Flag("ldflags", "add custom ldflags").StringVar(&Commands.Ldflags)
+	Commands.App.Flag("soft-float", "enable soft float for mips").BoolVar(&Commands.SoftFloat)
+	Commands.App.Flag("output", "output name").Short('o').StringVar(&Commands.OutputName)
+}
---
+++ internal/global/models/commands.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/alecthomas/kingpin/v2"
+
+type Commands struct {
+	App        *kingpin.Application
+	Target     string
+	Ldflags    string
+	SoftFloat  bool
+	OutputName string
+}
