b25dc8f102b08a870a66f623b0e84c4270a4eac2
Mmx233 <green.dusk8264@fastmail.com>
2024-03-27 15:37:53 +0800

    improve: add DecodeCompressPath to tools

--- tools/7z.go
+++ tools/7z.go
@@ -6,22 +6,46 @@ import (
 	"strings"
 )
 
-func MakeZip(filePath string, targetName string) error {
+type CompressInfo struct {
+	// src info
+	BaseName string
+	Ext      string
+	Name     string
+	Dir      string
+
+	// dist info
+	OutputPath string
+	TargetName string // rename src file
+}
+
+func DecodeCompressPath(filePath, targetName, targetFormat string) *CompressInfo {
 	baseName := path.Base(filePath)
 	ext := path.Ext(baseName)
 	name := strings.TrimSuffix(baseName, ext)
 	dir := strings.TrimSuffix(filePath, baseName)
 
-	outputPath := name + ".zip"
+	return &CompressInfo{
+		BaseName: baseName,
+		Ext:      ext,
+		Name:     name,
+		Dir:      dir,
 
-	cmd := exec.Command("7z", "a", "-tzip", outputPath, baseName, "-mx9")
-	cmd.Dir = dir
+		OutputPath: name + "." + targetFormat,
+		TargetName: targetName + ext,
+	}
+}
+
+func MakeZip(filePath, targetName string) error {
+	info := DecodeCompressPath(filePath, targetName, "zip")
+
+	cmd := exec.Command("7z", "a", "-tzip", info.OutputPath, info.BaseName, "-mx9")
+	cmd.Dir = info.Dir
 	err := cmd.Run()
 	if err != nil {
 		return err
 	}
 
-	cmd = exec.Command("7z", "rn", outputPath, baseName, targetName+ext)
-	cmd.Dir = dir
+	cmd = exec.Command("7z", "rn", info.OutputPath, info.BaseName, info.TargetName)
+	cmd.Dir = info.Dir
 	return cmd.Run()
 }
