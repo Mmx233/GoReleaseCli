4749bb7f38a189dcdfe1b26898f84d51da5cfca6
Mmx233 <green.dusk8264@fastmail.com>
2024-03-27 18:14:45 +0800

    improve: pretty cli help message

--- internal/global/commands.go
+++ internal/global/commands.go
@@ -15,17 +15,17 @@ func InitCommands(version string) {
 	Commands.App.VersionFlag.Short('v')
 	Commands.App.HelpFlag.Short('h')
 
-	Commands.App.Flag("thread", "use how many thread to build").Short('j').Default(fmt.Sprint(runtime.NumCPU() + 1)).Uint16Var(&Commands.Thread)
-	Commands.App.Flag("compress", "compress binary").Short('c').HintOptions("zip", "tar.gz").StringVar(&Commands.Compress)
+	Commands.App.Flag("thread", "How many threads to use for parallel compilation.").Short('j').Default(fmt.Sprint(runtime.NumCPU() + 1)).Uint16Var(&Commands.Thread)
+	Commands.App.Flag("compress", "Compress the binary into the specified format of compressed file.").Short('c').HintOptions("zip", "tar.gz").StringVar(&Commands.Compress)
 
-	Commands.App.Flag("ldflags", "add custom ldflags").StringVar(&Commands.Ldflags)
-	Commands.App.Flag("soft-float", "enable soft float for mips").BoolVar(&Commands.SoftFloat)
-	Commands.App.Flag("cgo", "enable cgo").BoolVar(&Commands.Cgo)
-	Commands.App.Flag("os", "target os").HintOptions("windows,linux").StringVar(&Commands.OS)
-	Commands.App.Flag("arch", "target arch").HintOptions("386,amd64").StringVar(&Commands.Arch)
+	Commands.App.Flag("ldflags", "Add custom ldflags.").StringVar(&Commands.Ldflags)
+	Commands.App.Flag("soft-float", "Enable soft float for mips.").BoolVar(&Commands.SoftFloat)
+	Commands.App.Flag("cgo", "Enable go cgo.").BoolVar(&Commands.Cgo)
+	Commands.App.Flag("os", "Target os").HintOptions("windows,linux").StringVar(&Commands.OS)
+	Commands.App.Flag("arch", "Target arch.").HintOptions("386,amd64").StringVar(&Commands.Arch)
 
-	Commands.App.Flag("output", "output dir path").Short('d').Default("build").StringVar(&Commands.Output)
-	Commands.App.Flag("name", "output binary file name").Short('o').StringVar(&Commands.Name)
-	Commands.App.Arg("target", "target package").Required().StringVar(&Commands.Target)
+	Commands.App.Flag("output", "Output dir path.").Short('d').Default("build").StringVar(&Commands.Output)
+	Commands.App.Flag("name", "Output binary file name.").Short('o').StringVar(&Commands.Name)
+	Commands.App.Arg("target", "Target package.").Required().StringVar(&Commands.Target)
 
 }
