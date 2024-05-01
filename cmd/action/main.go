2c2e4d1295f80bc0f39d2c94be5b6c65d22e4ebf
Mmx233 <green.dusk8264@fastmail.com>
2024-05-01 17:26:17 +0800

    chore: cancel specify workspace path in action

--- cmd/action/main.go
+++ cmd/action/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Mmx233/GoReleaseCli/internal/pkg/builder"
 	"github.com/caarlos0/env/v6"
 	log "github.com/sirupsen/logrus"
-	"path"
 	"runtime"
 )
 
@@ -18,7 +17,6 @@ func init() {
 	if global.Config.Thread == 0 {
 		global.Config.Thread = uint16(runtime.NumCPU() + 1)
 	}
-	global.Config.Output = path.Join("/github/workspace", global.Config.Output)
 	if err := global.Init(); err != nil {
 		log.Fatalln(err)
 	}
