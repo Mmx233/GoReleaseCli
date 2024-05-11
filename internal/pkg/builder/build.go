b88a79fc9715d0e8f1340fc2b31b9fb8d3366f88
Mmx233 <green.dusk8264@fastmail.com>
2024-05-11 15:54:41 +0800

    feat: download mod before build

--- internal/pkg/builder/build.go
+++ internal/pkg/builder/build.go
@@ -2,10 +2,14 @@ package builder
 
 import (
 	"github.com/Mmx233/GoReleaseCli/internal/global"
+	"github.com/Mmx233/GoReleaseCli/pkg/goCMD"
 	log "github.com/sirupsen/logrus"
 )
 
 func Run() {
+	if err := goCMD.DownloadMod().Run(); err != nil {
+		log.Fatalln("download go mod failed:", err)
+	}
 	builder, err := NewBuilder(global.Config.Output)
 	if err != nil {
 		log.Fatalln(err)
---
+++ pkg/goCMD/mod.go
@@ -0,0 +1,7 @@
+package goCMD
+
+import "os/exec"
+
+func DownloadMod() *exec.Cmd {
+	return exec.Command("go", "mod", "download")
+}
