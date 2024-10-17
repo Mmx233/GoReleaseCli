9aaee4f48c4a3a66b492d584e2c807ad3b2b5e07
Mmx <green.dusk8264@fastmail.com>
2024-10-17 14:43:41 +0800

    fix: default divider

--- internal/global/config.go
+++ internal/global/config.go
@@ -11,7 +11,7 @@ type _Config struct {
 	// If Platforms is set, OS and Arch will not be used.
 	Platforms string `env:"PLATFORMS"`
 	Output    string `env:"OUTPUT,notEmpty" envDefault:"build"`
-	Divider   string `env:"DIVIDER" envDefault:"-"`
+	Divider   string `env:"DIVIDER" envDefault:"_"`
 	Name      string `env:"NAME"`
 
 	ModDownloadArgs string `env:"MOD-DOWNLOAD-ARGS"`
