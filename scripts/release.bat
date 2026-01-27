f5f239b93d29b4b564e58ab1a51216f4e6ae6a8e
Mmx233 <36563672+Mmx233@users.noreply.github.com>
2026-01-27 18:20:23 +0800

    chore: remove scripts

--- scripts/release.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-SET /p v=Version:
-go run ./cmd/release ./cmd/release --ldflags="-X main.Version=%v%" --extra-arches -c tar.gz
--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-  echo "Version is required"
-  exit
-fi
-
-go run ./cmd/release ./cmd/release --ldflags="-X main.Version=${1}" --extra-arches -c tar.gz
